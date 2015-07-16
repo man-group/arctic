d52806611a0b748c2c128969ffe8c1c2aafcfccb
Artjoms Iskovs <aiskovs@ahl.com>
2015-07-16 10:34:12 +0100

    EASY-407 Parallel compression switch Allow switch off parallel lz4 from environmental variable EASY-407 Remove lz4hc Fix typo EASY-407 refactor flag name

--- arctic/_compression.py
+++ arctic/_compression.py
@@ -1,3 +1,5 @@
+import lz4
+import os
 from .logging import logger
 
 try:
@@ -7,34 +9,23 @@ except ImportError:
     import lz4 as clz4
 
 
-USE_LZ4HC = True  # switch to use LZ4HC. Default True
-LZ4HC_N_PARALLEL = 5  # No. of elements to use parellel compression in LZ4HC mode
+# switch to parallel LZ4 compress (and potentially other parallel stuff), Default True
+ENABLE_PARALLEL = not os.environ.get('DISABLE_PARALLEL')
 LZ4_N_PARALLEL = 50  # No. of elements to use parellel compression in LZ4 mode
 
 
-def use_lz4hc(mode):
+def enable_parallel_lz4(mode):
     """
-    Set the global LZ4HC mode
+    Set the global multithread compression mode
 
     Parameters
     ----------
         mode: `bool`
-            True: Use LZ4HC False: Use LZ4
+            True: Use parallel compression. False: Use sequential compression
     """
-    global USE_LZ4HC
-    USE_LZ4HC = mode
-    logger.info("Setting compression mode to %s" % ("LZ4HC" if mode else "LZ4 (no HC)"))
-
-
-def _should_use_lz4hc():
-    return USE_LZ4HC
-
-
-def _is_interactive_mode():
-    # http://stackoverflow.com/questions/2356399/tell-if-python-is-in-interactive-mode
-    # currently unused - but could in-future flip to LZ4 if in interactive mode
-    import __main__ as main
-    return not hasattr(main, '__file__')
+    global ENABLE_PARALLEL
+    ENABLE_PARALLEL = mode
+    logger.info("Setting parallelisation mode to {}".format("multithread" if mode else "singlethread"))
 
 
 def compress_array(str_list):
@@ -44,18 +35,20 @@ def compress_array(str_list):
     By default LZ4 mode is standard in interactive mode,
     and high compresion in applications/scripts
     """
-    if _should_use_lz4hc():
-        # Less than 5 chunks its quicker to compress sequentially..
-        if len(str_list) > LZ4HC_N_PARALLEL:
-            return clz4.compressarrHC(str_list)
-        else:
-            return [clz4.compressHC(s) for s in str_list]
+    if not ENABLE_PARALLEL:
+        return [lz4.compress(s) for s in str_list]
+
+    # Less than 50 chunks its quicker to compress sequentially..
+    if len(str_list) > LZ4_N_PARALLEL:
+        return clz4.compressarr(str_list)
     else:
-        # Less than 50 chunks its quicker to compress sequentially..
-        if len(str_list) > LZ4_N_PARALLEL:
-            return clz4.compressarr(str_list)
-        else:
-            return [clz4.compress(s) for s in str_list]
+        return [clz4.compress(s) for s in str_list]
+
+
+def _get_lib():
+    if ENABLE_PARALLEL:
+        return clz4
+    return lz4
 
 
 def compress(_str):
@@ -65,19 +58,20 @@ def compress(_str):
     By default LZ4 mode is standard in interactive mode,
     and high compresion in applications/scripts
     """
-    compressfn = clz4.compressHC if _should_use_lz4hc() else clz4.compress
-    return compressfn(_str)
+    return _get_lib().compress(_str)
 
 
 def decompress(_str):
     """
     Decompress a string
     """
-    return clz4.decompress(_str)
+    return _get_lib().decompress(_str)
 
 
 def decompress_array(str_list):
     """
     Decompress a list of strings
     """
-    return clz4.decompressarr(str_list)
+    if ENABLE_PARALLEL:
+        return clz4.decompressarr(str_list)
+    return [lz4.decompress(chunk) for chunk in str_list]
