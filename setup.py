4f74ccb034b6f0aaaddb22c85f8c94d34d30d1c3
James Blackburn <jblackburn@ahl.com>
2015-10-28 10:59:21 +0000

    Bump version number to 1.11.0

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ compress = Extension('arctic._compress',
 
 setup(
     name="arctic",
-    version="1.10.0",
+    version="1.11.0",
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
