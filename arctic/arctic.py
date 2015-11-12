5b7140a4b0efa77f6d5b8185b9f21380c05d6a1d
Artjoms Iskovs <aiskovs@ahl.com>
2015-11-12 16:05:05 +0000

    MDP-621 Wrap the connection initialisation in a mongo_retry decorator

--- arctic/arctic.py
+++ arctic/arctic.py
@@ -100,18 +100,19 @@ class Arctic(object):
             mongo_host.server_info()
             self.mongo_host = ",".join(["{}:{}".format(x[0], x[1]) for x in mongo_host.nodes])
             self._adminDB = self._conn.admin
-
+    
     @property
+    @mongo_retry
     def _conn(self):
         with self._lock:
             if self.__conn is None:
                 host = get_mongodb_uri(self.mongo_host)
                 logger.info("Connecting to mongo: {0} ({1})".format(self.mongo_host, host))
-                self.__conn = mongo_retry(pymongo.MongoClient)(host=host,
-                                                               maxPoolSize=self._MAX_CONNS,
-                                                               socketTimeoutMS=self._socket_timeout,
-                                                               connectTimeoutMS=self._connect_timeout,
-                                                               serverSelectionTimeoutMS=self._server_selection_timeout)
+                self.__conn = pymongo.MongoClient(host=host,
+                                                   maxPoolSize=self._MAX_CONNS,
+                                                   socketTimeoutMS=self._socket_timeout,
+                                                   connectTimeoutMS=self._connect_timeout,
+                                                   serverSelectionTimeoutMS=self._server_selection_timeout)
                 self._adminDB = self.__conn.admin
 
                 # Authenticate against admin for the user
