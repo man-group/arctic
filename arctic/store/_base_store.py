bfb67d41fbd1ba11e2cedeccfcdf296424c486c3
Bryant Moscon <bryant.moscon@numeric.com>
2015-11-23 20:08:29 +0000

    add base class

---
+++ arctic/store/_base_store.py
@@ -0,0 +1,12 @@
+class BaseStore(object):
+    def read(self, lib, version, symbol, **kwargs):
+        pass
+
+    def write(self, lib, version, symbol, item, previous_version):
+        pass
+
+    def get_info(self, lib, version, symbol, **kwargs):
+        pass
+
+    def get_data_info(self, version):
+        pass
