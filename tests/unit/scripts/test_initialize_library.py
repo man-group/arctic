aead1e9264b3e415f2e94525c8766be2b7d4ad2c
Bryant Moscon <bmoscon@gmail.com>
2016-01-18 09:32:00 -0500

    Fix issue with autospec

--- tests/unit/scripts/test_initialize_library.py
+++ tests/unit/scripts/test_initialize_library.py
@@ -5,12 +5,14 @@ from arctic.scripts import arctic_init_library as mil
 
 from ...util import run_as_main
 
+from arctic.scripts.arctic_init_library import Arctic as ar
+
 
 def test_init_library():
     # Create the user agains the current mongo database
     with patch('pymongo.MongoClient') as MongoClient, \
          patch('arctic.scripts.arctic_init_library.logger', autospec=True) as logger, \
-         patch('arctic.scripts.arctic_init_library.Arctic', autospec=True) as Arctic, \
+         patch('arctic.scripts.arctic_init_library.Arctic', spec=ar) as Arctic, \
          patch('arctic.scripts.arctic_init_library.get_mongodb_uri', autospec=True) as get_mongodb_uri, \
          patch('arctic.scripts.arctic_init_library.do_db_auth', autospec=True) as do_db_auth:
         run_as_main(mil.main, '--host', 'hostname', '--library', 'arctic_user.library', '--type', 'VersionStore')
@@ -26,10 +28,10 @@ def test_init_library():
 def test_init_library_no_admin():
     # Create the user agains the current mongo database
     with patch('pymongo.MongoClient') as MongoClient, \
-         patch('arctic.scripts.arctic_init_library.logger'), \
-         patch('arctic.scripts.arctic_init_library.Arctic') as Arctic, \
-         patch('arctic.scripts.arctic_init_library.get_mongodb_uri') as get_mongodb_uri, \
-         patch('arctic.scripts.arctic_init_library.do_db_auth') as do_db_auth:
+         patch('arctic.scripts.arctic_init_library.logger', autospec=True), \
+         patch('arctic.scripts.arctic_init_library.Arctic', spec=ar) as Arctic, \
+         patch('arctic.scripts.arctic_init_library.get_mongodb_uri', autospec=True) as get_mongodb_uri, \
+         patch('arctic.scripts.arctic_init_library.do_db_auth', autospec=True) as do_db_auth:
         run_as_main(mil.main, '--host', 'hostname', '--library', 'arctic_user.library', '--type', 'VersionStore')
 
     get_mongodb_uri.assert_called_once_with('hostname')
@@ -41,10 +43,10 @@ def test_init_library_no_admin():
 def test_init_library_hashed():
     # Create the user agains the current mongo database
     with patch('pymongo.MongoClient') as MongoClient, \
-         patch('arctic.scripts.arctic_init_library.logger') as logger, \
-         patch('arctic.scripts.arctic_init_library.Arctic') as Arctic, \
-         patch('arctic.scripts.arctic_init_library.get_mongodb_uri') as get_mongodb_uri, \
-         patch('arctic.scripts.arctic_init_library.do_db_auth') as do_db_auth:
+         patch('arctic.scripts.arctic_init_library.logger', autospec=True) as logger, \
+         patch('arctic.scripts.arctic_init_library.Arctic', spec=ar) as Arctic, \
+         patch('arctic.scripts.arctic_init_library.get_mongodb_uri', autospec=True) as get_mongodb_uri, \
+         patch('arctic.scripts.arctic_init_library.do_db_auth', autospec=True) as do_db_auth:
         run_as_main(mil.main, '--host', 'hostname', '--library', 'arctic_user.library', '--type', 'VersionStore', '--hashed')
 
     get_mongodb_uri.assert_called_once_with('hostname')
@@ -57,10 +59,10 @@ def test_init_library_hashed():
 
 def test_init_library_no_admin_no_user_creds():
     with patch('pymongo.MongoClient') as MongoClient, \
-         patch('arctic.scripts.arctic_init_library.logger') as logger, \
-         patch('arctic.scripts.arctic_init_library.Arctic') as Arctic, \
-         patch('arctic.scripts.arctic_init_library.get_mongodb_uri') as get_mongodb_uri, \
-         patch('arctic.scripts.arctic_init_library.do_db_auth', return_value=False) as do_db_auth:
+         patch('arctic.scripts.arctic_init_library.logger', autospec=True) as logger, \
+         patch('arctic.scripts.arctic_init_library.Arctic', spec=ar) as Arctic, \
+         patch('arctic.scripts.arctic_init_library.get_mongodb_uri', autospec=True) as get_mongodb_uri, \
+         patch('arctic.scripts.arctic_init_library.do_db_auth', return_value=False, autospec=True) as do_db_auth:
 
         MongoClient.return_value['arctic_user'].authenticate.return_value = False
         run_as_main(mil.main, '--host', 'hostname', '--library', 'arctic_user.library', '--type', 'VersionStore')
