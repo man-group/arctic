e2838de0262e3c1070d7189dd6a000db72f60836
Bryant Moscon <bmoscon@gmail.com>
2016-03-29 10:12:43 -0400

    Update _parse.py

--- arctic/date/_parse.py
+++ arctic/date/_parse.py
@@ -2,9 +2,4 @@ from dateutil.parser import parse as _parse
 
 
 def parse(string, agnostic=False, **kwargs):
-    parsed = _parse(string, **kwargs)
-    if agnostic or (parsed == _parse(string, yearfirst=True, **kwargs)
-                           == _parse(string, dayfirst=True, **kwargs)):
-        return parsed
-    else:
-        raise ValueError("The date was ambiguous: %s" % string)
+    return _parse(string, yearfirst=True, dayfirst=False, **kwargs)
