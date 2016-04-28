f91ac75adeb072c84561978156aee4ced2e2e8ae
Bryant Moscon <bryant.moscon@numeric.com>
2016-04-28 21:06:39 +0100

    New chunkstore storage type

--- arctic/serialization/pandas_serializer.py
+++ arctic/serialization/pandas_serializer.py
@@ -11,16 +11,43 @@ log = logging.getLogger(__name__)
 DTN64_DTYPE = 'datetime64[ns]'
 
 
-def _to_primitive(arr):
+def serialize(item, string_max_len=None):
+    if isinstance(item, Series):
+        return SeriesSerializer().serialize(item, string_max_len=string_max_len)
+    if isinstance(item, DataFrame):
+        return DataFrameSerializer().serialize(item, string_max_len=string_max_len)
+    else:
+        raise Exception("Cannot serialize data of type %s" % (type(item)))
+
+
+def deserialize(item, dtype):
+    if dtype == SeriesSerializer.TYPE:
+        return SeriesSerializer().deserialize(item)
+    if dtype == DataFrameSerializer.TYPE:
+        return DataFrameSerializer().deserialize(item)
+    else:
+        raise Exception("Cannot deserialize data of type %s" % (dtype))
+
+
+def _to_primitive(arr, string_max_len=None):
     if arr.dtype.hasobject:
         if len(arr) > 0:
             if isinstance(arr[0], Timestamp):
                 return np.array([t.value for t in arr], dtype=DTN64_DTYPE)
+        if string_max_len:
+            return np.array(arr.astype('U{:d}'.format(string_max_len)))
         return np.array(list(arr))
     return arr
 
 
 class PandasSerializer(object):
+
+    def _dtype(self, string, metadata=None):
+        if metadata is None:
+            metadata = {}
+        if string.startswith('['):
+            return np.dtype(eval(string), metadata=metadata)
+        return np.dtype(string, metadata=metadata)
 
     def _index_to_records(self, df):
         metadata = {}
@@ -70,13 +97,16 @@ class PandasSerializer(object):
 
         return rtn
 
-    def _to_records(self, df):
+    def _to_records(self, df, string_max_len=None):
         """
         Similar to DataFrame.to_records()
         Differences:
             Attempt type conversion for pandas columns stored as objects (e.g. strings),
             as we can only store primitives in the ndarray.
             Use dtype metadata to store column and index names.
+        
+        string_max_len: integer - enforces a string size on the dtype, if any
+                                  strings exist in the record
         """
 
         index_names, ix_vals, metadata = self._index_to_records(df)
@@ -84,8 +114,11 @@ class PandasSerializer(object):
 
         metadata['columns'] = columns
         names = index_names + columns
-        arrays = ix_vals + column_vals
-        arrays = list(map(_to_primitive, arrays))
+
+        arrays = []
+        for arr in ix_vals + column_vals:
+            arrays.append(_to_primitive(arr, string_max_len))
+
         dtype = np.dtype([(str(x), v.dtype) if len(v.shape) == 1 else (str(x), v.dtype, v.shape[1]) for x, v in zip(names, arrays)],
                          metadata=metadata)
 
@@ -122,7 +155,9 @@ class PandasSerializer(object):
     def deserialize(self, item):
         pass
 
+
 class SeriesSerializer(PandasSerializer):
+    TYPE = 'series'
 
     def _column_data(self, s):
         if s.name is None:
@@ -136,11 +171,12 @@ class SeriesSerializer(PandasSerializer):
         name = item.dtype.names[-1]
         return Series.from_array(item[name], index=index, name=name)
 
-    def serialize(self, item):
-        return self._to_records(item)
+    def serialize(self, item, string_max_len=None):
+        return self._to_records(item, string_max_len)
 
 
 class DataFrameSerializer(PandasSerializer):
+    TYPE = 'df'
 
     def _column_data(self, df):
         columns = list(map(str, df.columns))
@@ -159,5 +195,5 @@ class DataFrameSerializer(PandasSerializer):
         columns = item.dtype.metadata['columns']
         return DataFrame(data=item[column_fields], index=index, columns=columns)
 
-    def serialize(self, item):
-        return self._to_records(item)
+    def serialize(self, item, string_max_len=None):
+        return self._to_records(item, string_max_len)
