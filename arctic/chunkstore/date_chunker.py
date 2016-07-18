2b619e11e39681d353ddc11ba3e2f0f5bb846fb7
Bryant Moscon <bryant.moscon@numeric.com>
2016-07-18 18:13:54 +0100

    Address code review comments

--- arctic/chunkstore/date_chunker.py
+++ arctic/chunkstore/date_chunker.py
@@ -74,17 +74,17 @@ class DateChunker(Chunker):
         return self._get_date_range(data)
 
     def to_mongo(self, range_obj):
-        if range_obj[0] and range_obj[1]:
-            return {'$and': [{'start': {'$lte': range_obj[1]}}, {'end': {'$gte': range_obj[0]}}]}
-        elif range_obj[0]:
-            return {'end': {'$gte': range_obj[0]}}
-        elif range_obj[1]:
-            return {'start': {'$lte': range_obj[1]}}
+        if range_obj.start and range_obj.end:
+            return {'$and': [{'start': {'$lte': range_obj.end}}, {'end': {'$gte': range_obj.start}}]}
+        elif range_obj.start:
+            return {'end': {'$gte': range_obj.start}}
+        elif range_obj.end:
+            return {'start': {'$lte': range_obj.end}}
         else:
             return {}
 
     def filter(self, data, range_obj):
-        return data.ix[range_obj[0]:range_obj[1]]
+        return data.ix[range_obj.start:range_obj.end]
 
     def exclude(self, data, range_obj):
-        return data[(data.index.get_level_values('date') < range_obj[0]) | (data.index.get_level_values('date') > range_obj[1])]
+        return data[(data.index.get_level_values('date') < range_obj.start) | (data.index.get_level_values('date') > range_obj.end)]
