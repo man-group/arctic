2e27b1c284f0f775c8af00baa5684d2028a5ae97
PAN, Myautsai <mckelvin@users.noreply.github.com>
2016-10-28 23:03:28 +0800

    TickStore.delete: Return result of deletion

--- tests/integration/tickstore/test_ts_delete.py
+++ tests/integration/tickstore/test_ts_delete.py
@@ -23,13 +23,21 @@ def test_delete(tickstore_lib):
               ]
     tickstore_lib._chunk_size = 1
     tickstore_lib.write('SYM', DUMMY_DATA)
-    tickstore_lib.delete('SYM')
+    deleted = tickstore_lib.delete('SYM')
+    assert deleted.deleted_count == 2
     with pytest.raises(NoDataFoundException):
         tickstore_lib.read('SYM', date_range=DateRange(20130102), columns=None)
 
     # Delete with a date-range
     tickstore_lib.write('SYM', DUMMY_DATA)
-    tickstore_lib.delete('SYM', DateRange(dt(2013, 1, 1, tzinfo=mktz('Europe/London')), dt(2013, 1, 2, tzinfo=mktz('Europe/London'))))
+    deleted = tickstore_lib.delete(
+        'SYM',
+        DateRange(
+            dt(2013, 1, 1, tzinfo=mktz('Europe/London')),
+            dt(2013, 1, 2, tzinfo=mktz('Europe/London'))
+        )
+    )
+    assert deleted.deleted_count == 1
     df = tickstore_lib.read('SYM', columns=None)
     assert np.allclose(df['b'].values, np.array([4.]))
 
@@ -49,6 +57,14 @@ def test_delete_daterange(tickstore_lib):
     tickstore_lib.write('SYM', DUMMY_DATA)
 
     # Delete with a date-range
-    tickstore_lib.delete('SYM', DateRange(dt(2013, 1, 1, tzinfo=mktz('Europe/London')), dt(2013, 2, 1, tzinfo=mktz('Europe/London')), CLOSED_OPEN))
+    deleted = tickstore_lib.delete(
+        'SYM',
+        DateRange(
+            dt(2013, 1, 1, tzinfo=mktz('Europe/London')),
+            dt(2013, 2, 1, tzinfo=mktz('Europe/London')),
+            CLOSED_OPEN
+        )
+    )
+    assert deleted.deleted_count == 1
     df = tickstore_lib.read('SYM', columns=None)
     assert np.allclose(df['b'].values, np.array([4.]))
