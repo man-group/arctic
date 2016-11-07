f3859a1c17571537f850917da7fe3e7bb821d012
AHL Development <ahldev@ahl.com>
2016-11-07 15:34:34 +0000

    Rolling version number 1.33.0 -> 1.34.0

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ compress = Extension('arctic._compress',
 
 setup(
     name="arctic",
-    version="1.33.0",
+    version="1.34.0",
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
