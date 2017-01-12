c2dc485736d9a04fc6634098089fafc4b1b5dd22
Bryant Moscon <bmoscon@gmail.com>
2017-01-12 15:07:26 -0500

    Update version_store.py

--- arctic/store/version_store.py
+++ arctic/store/version_store.py
@@ -795,7 +795,7 @@ class VersionStore(object):
             sharding = conn.config.databases.find_one({'_id': db.name})
             if sharding:
                 res['sharding'].update(sharding)
-            res['sharding']['collections'] = list(conn.config.collections.find({'_id': {'$regex': '^' + db.name + '\..*'}}))
+            res['sharding']['collections'] = list(conn.config.collections.find({'_id': {'$regex': '^' + db.name + r"\..*"}}))
         except OperationFailure:
             # Access denied
             pass
