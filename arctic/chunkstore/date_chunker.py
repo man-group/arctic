65fd950586c30756f4d92178f96c233d3f21f15f
bmoscon <bmoscon@gmail.com>
2017-02-13 21:16:07 +0000

    Fix to work with old versions of pandas

--- arctic/chunkstore/date_chunker.py
+++ arctic/chunkstore/date_chunker.py
@@ -29,7 +29,7 @@ class DateChunker(Chunker):
         elif 'date' in df.columns:
             dates = pd.DatetimeIndex(df.date)
             if not dates.is_monotonic_increasing:
-                df = df.sort_values(by='date')
+                df = df.sort(columns='date')
                 dates = pd.DatetimeIndex(df.date)
         else:
             raise Exception("Data must be datetime indexed or have a column named 'date'")
