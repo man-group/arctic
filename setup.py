b89e950feddba3734b45c8170c62151002b1b02a
Bryant Moscon <bmoscon@gmail.com>
2018-04-16 15:02:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ def extensions():
 
 setup(
     name="arctic",
-    version="1.64.0",
+    version="1.65.0",
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
