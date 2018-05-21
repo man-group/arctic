4b1d479ce8b566d64978eeda7003b4b54c9baae1
AHL Development <ahldev@ahl.com>
2018-05-21 11:32:26 +0100

    Rolling version number 1.66.0 -> 1.67.0

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ def extensions():
 
 setup(
     name="arctic",
-    version="1.66.0",
+    version="1.67.0",
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
