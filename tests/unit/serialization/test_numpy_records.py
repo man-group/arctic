24a7fb7a8a347d6b1a1767ef2d947600d27299d3
Dimosthenis Pediaditakis <dimosthenes@gmail.com>
2018-10-26 10:08:27 +0100

    complete implementation and tests for the fast is-serializable check, turned off by default (#643)

--- tests/unit/serialization/test_numpy_records.py
+++ tests/unit/serialization/test_numpy_records.py
@@ -1,57 +1,102 @@
 import numpy as np
-
-from numpy.testing import assert_array_equal
+import pytest
 from mock import patch, Mock, sentinel
-from arctic.serialization.numpy_records import PandasSerializer, _to_primitive
+from numpy.testing import assert_array_equal
 from pandas import Timestamp
+
+import arctic.serialization.numpy_records as anr
 
 
 def test_to_primitive_timestamps():
-    arr = _to_primitive(np.array([Timestamp('2010-11-12 00:00:00')]))
+    arr = anr._to_primitive(np.array([Timestamp('2010-11-12 00:00:00')]))
     assert_array_equal(arr, np.array([Timestamp('2010-11-12 00:00:00').value], dtype='datetime64[ns]'))
 
-def test_can_convert_to_records_without_objects_returns_false_on_exception_in_to_records():
-    store = PandasSerializer()
-    store._to_records = Mock(side_effect=TypeError('uhoh'))
 
-    with patch('arctic.serialization.numpy_records.log') as mock_log:
-        assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is False
+@pytest.mark.parametrize("fast_serializable_check", (True, False))
+def test_can_convert_to_records_without_objects_returns_false_on_exception_in_to_records(fast_serializable_check):
+    orig_config = anr._FAST_CHECK_DF_SERIALIZABLE
+    try:
+        anr.set_fast_check_df_serializable(fast_serializable_check)
+        store = anr.PandasSerializer()
+        mymock = Mock(side_effect=TypeError('uhoh'))
+        if fast_serializable_check:
+            store.fast_check_serializable = mymock
+        else:
+            store._to_records = mymock
+        with patch('arctic.serialization.numpy_records.log') as mock_log:
+            assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is False
 
-    mock_log.info.assert_called_once_with('Pandas dataframe my_symbol caused exception "TypeError(\'uhoh\',)"'
-                                          ' when attempting to convert to records. Saving as Blob.')
-    store._to_records.assert_called_once_with(sentinel.df)
-
-
-def test_can_convert_to_records_without_objects_returns_false_when_records_have_object_dtype():
-    store = PandasSerializer()
-    store._to_records = Mock(return_value=(np.array(['a', 'b', None, 'd']), None))
-
-    with patch('arctic.serialization.numpy_records.log') as mock_log:
-        assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is False
-
-    mock_log.warning.assert_called_once_with('Pandas dataframe my_symbol contains Objects, saving as Blob')
-    store._to_records.assert_called_once_with(sentinel.df)
-
-
-def test_can_convert_to_records_without_objects_returns_false_when_records_have_arrays_in_them():
-    store = PandasSerializer()
-    store._to_records = Mock(return_value=(np.rec.array([(1356998400000000000, ['A', 'BC'])],
-                                                       dtype=[('index', '<M8[ns]'), ('values', 'S2', (2,))]), None))
-
-    with patch('arctic.serialization.numpy_records.log') as mock_log:
-        assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is False
-
-    mock_log.info.assert_called_once_with('Pandas dataframe my_symbol contains >1 dimensional arrays, saving as Blob')
-    store._to_records.assert_called_once_with(sentinel.df)
+        mock_log.warning.assert_called_once_with('Pandas dataframe my_symbol caused exception "TypeError(\'uhoh\',)" '
+                                                 'when attempting to convert to records. Saving as Blob.')
+        if fast_serializable_check:
+            store.fast_check_serializable.assert_called_once_with(sentinel.df)
+        else:
+            store._to_records.assert_called_once_with(sentinel.df)
+    finally:
+        anr._FAST_CHECK_DF_SERIALIZABLE = orig_config
 
 
-def test_can_convert_to_records_without_objects_returns_true_otherwise():
-    store = PandasSerializer()
-    store._to_records = Mock(return_value=(np.rec.array([(1356998400000000000, 'a')],
-                                                       dtype=[('index', '<M8[ns]'), ('values', 'S2')]), None))
+@pytest.mark.parametrize("fast_serializable_check", (True, False))
+def test_can_convert_to_records_without_objects_returns_false_when_records_have_object_dtype(fast_serializable_check):
+    orig_config = anr._FAST_CHECK_DF_SERIALIZABLE
+    try:
+        anr.set_fast_check_df_serializable(fast_serializable_check)
+        store = anr.PandasSerializer()
+        mymock = Mock(return_value=(np.array(['a', 'b', None, 'd']), None))
+        if fast_serializable_check:
+            store.fast_check_serializable = mymock
+        else:
+            store._to_records = mymock
+        with patch('arctic.serialization.numpy_records.log') as mock_log:
+            assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is False
+        mock_log.warning.assert_called_once_with('Pandas dataframe my_symbol contains Objects, saving as Blob')
+        if fast_serializable_check:
+            store.fast_check_serializable.assert_called_once_with(sentinel.df)
+        else:
+            store._to_records.assert_called_once_with(sentinel.df)
+    finally:
+        anr._FAST_CHECK_DF_SERIALIZABLE = orig_config
 
-    with patch('arctic.serialization.numpy_records.log') as mock_log:
-        assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is True
 
-    assert mock_log.info.call_count == 0
-    store._to_records.assert_called_once_with(sentinel.df)
+@pytest.mark.parametrize("fast_serializable_check", (True, False))
+def test_can_convert_to_records_without_objects_returns_false_when_records_have_arrays_in_them(fast_serializable_check):
+    orig_config = anr._FAST_CHECK_DF_SERIALIZABLE
+    try:
+        anr.set_fast_check_df_serializable(fast_serializable_check)
+        store = anr.PandasSerializer()
+        mymock = Mock(return_value=(np.rec.array([(1356998400000000000, ['A', 'BC'])], dtype=[('index', '<M8[ns]'), ('values', 'S2', (2,))]), None))
+        if fast_serializable_check:
+            store.fast_check_serializable = mymock
+        else:
+            store._to_records = mymock
+        with patch('arctic.serialization.numpy_records.log') as mock_log:
+            assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is False
+        mock_log.warning.assert_called_once_with('Pandas dataframe my_symbol contains >1 dimensional arrays, saving as Blob')
+        if fast_serializable_check:
+            store.fast_check_serializable.assert_called_once_with(sentinel.df)
+        else:
+            store._to_records.assert_called_once_with(sentinel.df)
+    finally:
+        anr._FAST_CHECK_DF_SERIALIZABLE = orig_config
+
+
+@pytest.mark.parametrize("fast_serializable_check", (True, False))
+def test_can_convert_to_records_without_objects_returns_true_otherwise(fast_serializable_check):
+    orig_config = anr._FAST_CHECK_DF_SERIALIZABLE
+    try:
+        anr.set_fast_check_df_serializable(fast_serializable_check)
+        store = anr.PandasSerializer()
+        mymock = Mock(return_value=(np.rec.array([(1356998400000000000, 'a')], dtype=[('index', '<M8[ns]'), ('values', 'S2')]), None))
+        if fast_serializable_check:
+            store.fast_check_serializable = mymock
+        else:
+            store._to_records = mymock
+        with patch('arctic.serialization.numpy_records.log') as mock_log:
+            assert store.can_convert_to_records_without_objects(sentinel.df, 'my_symbol') is True
+        assert mock_log.warning.call_count == 0
+        if fast_serializable_check:
+            store.fast_check_serializable.assert_called_once_with(sentinel.df)
+        else:
+            store._to_records.assert_called_once_with(sentinel.df)
+    finally:
+        anr._FAST_CHECK_DF_SERIALIZABLE = orig_config
