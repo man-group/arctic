e1cbc8ed6a6e221bc2fa331974f61c9d386ac205
Shashank Khare <shashank88@gmail.com>
2019-03-26 16:56:59 +0000

    Revert "Issue 710: Initialize SEGMENT_SHA in versions for writes and appends"
    This reverts commit 7e41511a9e334a1039c420c78ccdf1f32df76588.
    
    This was an incorrect fix as it doesn't handle cases of moving
    from disabled -> hybrid/fwd mode.

--- arctic/store/_ndarray_store.py
+++ arctic/store/_ndarray_store.py
@@ -409,9 +409,6 @@ class NdarrayStore(object):
 
         version['type'] = self.TYPE
         version[FW_POINTERS_CONFIG_KEY] = ARCTIC_FORWARD_POINTERS_CFG.name
-        # Create an empty entry to prevent cases where this field is accessed without being there. (#710)
-        if version[FW_POINTERS_CONFIG_KEY] != FwPointersCfg.DISABLED.name:
-            version[FW_POINTERS_REFS_KEY] = list()
 
         if str(dtype) != previous_version['dtype'] or \
                 _fw_pointers_convert_append_to_write(previous_version):
@@ -614,9 +611,6 @@ class NdarrayStore(object):
         version['up_to'] = len(item)
         version['sha'] = self.checksum(item)
         version[FW_POINTERS_CONFIG_KEY] = ARCTIC_FORWARD_POINTERS_CFG.name
-        # Create an empty entry to prevent cases where this field is accessed without being there. (#710)
-        if version[FW_POINTERS_CONFIG_KEY] != FwPointersCfg.DISABLED.name:
-            version[FW_POINTERS_REFS_KEY] = list()
 
         if previous_version:
             if 'sha' in previous_version \
