d6c271b957808dbf8172bcc54571eda8da7614f4
Bryant Moscon <bmoscon@gmail.com>
2019-12-06 11:11:37 -0500

    Update version for release

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class PyTest(TestCommand):
 
 setup(
     name="arctic",
-    version="1.79.2",
+    version="1.79.3",
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description=("AHL Research Versioned TimeSeries and Tick store"),
