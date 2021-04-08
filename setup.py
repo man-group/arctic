78a8e3a319541cade823d1d0d8c599481e09c3c9
Shashank Khare <shashank88@gmail.com>
2021-04-08 11:47:49 +0100

    Pin Pandas to check ck build

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
                       "enum-compat",
                       "futures; python_version == '2.7'",
                       "mockextras",
-                      "pandas",
+                      "pandas<=1.0.3",
                       "pymongo>=3.6.0",
                       "python-dateutil",
                       "pytz",
