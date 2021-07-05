9166d939021fdd3018791b849b02ef914ba722ea
ajvogel <ajvogel@gmail.com>
2021-07-05 09:22:19 +0200

    Remove restriction on pandas version
    Removing the restriction on pandas version since the Panel fixes should make it no longer necessary. Various people have reported arctic working latest version of Pandas.

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
                       "enum-compat",
                       "futures; python_version == '2.7'",
                       "mockextras",
-                      "pandas<=1.0.3",
+                      "pandas",
                       "pymongo>=3.6.0",
                       "python-dateutil",
                       "pytz",
