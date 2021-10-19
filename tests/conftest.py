d90f2e74c6914c027210410c2e552a3d190576a6
Kerr, Duncan (London) <Duncan.Kerr@man.com>
2021-10-19 11:26:18 +0100

    config.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import warnings
 
 # Turn deprecation warnings into errors
-warnings.simplefilter('error', DeprecationWarning)
+#warnings.simplefilter('error', DeprecationWarning)
 
 pytest_plugins = ['arctic.fixtures.arctic']
