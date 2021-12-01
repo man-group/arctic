48a98496ca211ee04923d0a8ee961ee7d81e61bc
Kerr, Duncan (London) <Duncan.Kerr@man.com>
2021-12-01 10:56:39 +0000

    fixed mongo_count tests

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -1,7 +1,8 @@
 from mock import MagicMock, ANY, patch
 
-from arctic._util import are_equals, enable_sharding, mongo_count, _use_new_count_api
+from arctic._util import are_equals, enable_sharding, mongo_count
 from arctic.arctic import Arctic
+import arctic._util
 
 
 def test_are_equals_not_df():
@@ -16,21 +17,25 @@ def test_enable_sharding_hashed():
     m._conn.admin.command.assert_called_with('shardCollection', ANY, key={'symbol': 'hashed'})
 
 
-def test_mongo_count_old_pymongo():
-    _use_new_count_api = None
+def test_mongo_count_old_pymongo(monkeypatch):
+    monkeypatch.setattr(arctic._util, '_use_new_count_api', None)
     with patch('pymongo.version', '3.6.0'):
         coll = MagicMock()
         mongo_count(coll, filter="_id:1")
         mongo_count(coll, filter={})
         mongo_count(coll)
+        assert coll.estimated_document_count.call_count == 0
+        assert coll.count_documents.call_count == 0
         assert coll.count.call_count == 3
 
-def test_mongo_count_new_pymongo():
-    _use_new_count_api = None
+
+def test_mongo_count_new_pymongo(monkeypatch):
+    monkeypatch.setattr(arctic._util, '_use_new_count_api', None)
     with patch('pymongo.version', '3.11.0'):
         coll2 = MagicMock()
         mongo_count(coll2, filter="_id:1")
         mongo_count(coll2, filter={})
         mongo_count(coll2)
         assert coll2.estimated_document_count.call_count == 2
-        coll2.count_documents.assert_called_once()
+        assert coll2.count_documents.call_count == 1
+        assert coll2.count.call_count == 0
