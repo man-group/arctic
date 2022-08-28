237469fbcd3453253ee94fe60eae2ab5cb71ee1d
duncan <duncan.kerr@live.com>
2022-08-28 13:13:54 +0100

    working circle build

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ class PyTest(TestCommand):
 
 
 setup( 
-    #safe_zip=True, 
     cmdclass={'test': PyTest},
     long_description='\n'.join((open('README.md').read(), open('CHANGES.md').read()))
     )
