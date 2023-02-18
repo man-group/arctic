a93c1743e124a611ca8ee68c42cc63e0a6476be0
Enrico <enrico.detoma@gmail.com>
2023-02-18 19:13:26 +0100

    pymongo <= 3.13.0

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
                       "mockextras",
                       "pandas",
                       "numpy",
-                      "pymongo>=3.6.0, <= 3.11.0",
+                      "pymongo>=3.6.0, <= 3.13.0",
                       "pytz",
                       "tzlocal",
                       "lz4",
