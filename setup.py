bb93951fd995be391577e43566d695d10b04d824
Kerr, Duncan (London) <Duncan.Kerr@man.com>
2023-05-12 18:20:32 +0100

    fix circleci tzlocal==4.4b1

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
                       "numpy<1.19.0",
                       "pymongo>=3.6.0, <= 3.11.0",
                       "pytz",
-                      #"tzlocal<=4.4", # required for circleCI builds
+                      "tzlocal", #"tzlocal<=4.4b1", # required for circleCI builds
                       "lz4",
                      ],
     # Note: pytest >= 4.1.0 is not compatible with pytest-cov < 2.6.1.
