39953e08b6f67c261144add88bf3ee0727352cf1
Kerr, Duncan (London) <Duncan.Kerr@man.com>
2023-06-30 16:40:39 +0100

    tidy

--- setup.py
+++ setup.py
@@ -113,7 +113,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Operating System :: POSIX",
         "Operating System :: MacOS",
